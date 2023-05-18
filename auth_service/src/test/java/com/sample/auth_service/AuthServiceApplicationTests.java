709f58534f48a0dc9f031312688172a0b2e7ec7f
mayank <yaduvanshimayank042@gmail.com>
2023-05-18 07:42:39 +0530

    initial commit

---
+++ auth_service/src/test/java/com/sample/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
